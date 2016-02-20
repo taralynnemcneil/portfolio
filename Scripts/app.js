04745b03ddd827f2f01ff16bcad79978ba7e0275
taralynnemcneil <taralynnemcneil@gmail.com>
2016-02-19 21:23:20 -0500

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,95 @@
+(function () {
+    /* ==============================================
+        SERVICES
+    ============================================== */
+    var replaceServicesOne = function () {
+        var servicesOne;
+        servicesOne = document.getElementById("servicesOne");
+        servicesOne.innerHTML = "Experienced in all type of print design from logos and stationary to brochures and package design.";
+    };
+
+    replaceServicesOne();
+    // end of services one
+    
+    var replaceServicesTwo = function () {
+        var servicesTwo;
+        servicesTwo = document.getElementById("servicesTwo");
+        servicesTwo.innerHTML = "Trained in web layouts, banner ads and web ready photos. With some knowledge of flash animation banners.";
+    };
+
+    replaceServicesTwo();
+    // end of services two
+    
+    var replaceServicesThree = function () {
+        var servicesThree;
+        servicesThree = document.getElementById("servicesThree");
+        servicesThree.innerHTML = "Hastily training in web development at Georgian College. Learning HTML, CSS, JavaScript, JQuery, PHP, etc..."
+    };
+
+    replaceServicesThree();
+    // end of services three
+    
+    /* ==============================================
+        ABOUT
+    ============================================== */
+    var replaceLeadAbout = function () {
+        var leadAbout;
+        leadAbout = document.getElementById('leadAbout');
+        leadAbout.innerHTML = "Hi, I'm Tara, a twenty three year old designer/developer from Barrie, ON. I have graduated from Georgian College with an Advanced Diploma in Graphic Design and I am on my way to graduating the Web Development program.";
+    };
+
+    replaceLeadAbout();
+    // end of lead about
+    
+    var replaceSubAbout = function () {
+        var subAbout;
+        subAbout = document.getElementById("subAbout");
+        subAbout.innerHTML = "A little about my past; I was born and raised in the town of Smiths Falls, ON. I moved to Barrie in 2011 to attend the graphic design program. In order to complete the program we had to participate in a 4 week placement. So I decided to go back to my home town and work for my mothers design company, PDG Media, where I worked on projects for clients around the community. I graduated the graphic design program in June 2015 and in September 2015, I decided to return to school for web development because I enjoyed the web design part of the graphic design program. ";
+    };
+
+    replaceSubAbout();
+    // end of sub about
+   
+    /* ==============================================
+         WEB DEVELOPMENT SKILLS
+     ============================================== */
+    var replaceWebMain = function () {
+        var webMain;
+        webMain = document.getElementById('webMain');
+        webMain.innerHTML = "Don't let the bar graph fool you.";
+    };
+
+    replaceWebMain();
+    // end of web main
+    
+    var replaceWebSub = function () {
+        var webSub;
+        webSub = document.getElementById('webSub');
+        webSub.innerHTML = "The graph may look bad, but as I said before, I am currently in school for web development. I am a quick learner and I really enjoy learning new things. So even though the bars are a little low on the right side they won't be for long. I am proficient in most coding software (eg. visual studio code, brackets, notepad++, textwrangler, etc...) and if I don't know the program I will be right on top of learning it.";
+    };
+
+    replaceWebSub();
+    // end of web sub
+    
+    /* ==============================================
+        GRAPHIC DESIGN SKILLS
+    ============================================== */
+    var replaceGdMain = function () {
+        var gdMain;
+        gdMain = document.getElementById('gdMain');
+        gdMain.innerHTML = "Now that's more like it. Here I know what I'm doing.";
+    };
+
+    replaceGdMain();
+    // end of gd main
+    
+    var replaceGdSub = function () {
+        var gdSub;
+        gdSub = document.getElementById('gdSub');
+        gdSub.innerHTML = "I have been using these programs since I was in the yearbook course in highschool. Working on photo manipulations, illustrations and document layouts. I also have some knowledge in Wordpress and working in Dreamweaver although I personally prefer alternative programs.";
+    };
+
+    replaceGdSub();
+    // end of gd sub
+
+})();
---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php
+
+// send ourselves an email using PHP
+// mail('taralynnemcneil@gmail.com', '1st Wed PHP email', 'Trying to send an email from a php page');
+
+mail('taralynnemcneil@gmail.com', '1st Wed PHP email', 'Trying to send an email from a php page', 'From: contact@georgiancollege.ca');
+
+echo 'go check your inbox!;'
+
+?>
